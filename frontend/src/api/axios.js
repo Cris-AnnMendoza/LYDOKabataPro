db11b57691847c8928067ddc7196f56dfcd509f4
Mendoza Cris-Ann F. <toothiechan@gmail.com>
2026-09-11 19:54:01 +0800

    Delete frontend directory

--- frontend/src/api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: 'http://localhost/lydo-system/backend/api',
-  withCredentials: true,          // send PHP session cookie
-  headers: { 'Content-Type': 'application/json' },
-})
-
-export default api
